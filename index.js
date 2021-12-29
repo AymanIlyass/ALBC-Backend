ef97c39ca85021c0bb849acfbb784cd00aca3ec4
Ayman EL Mouss <elmouss.ayman0@gmail.com>
2021-12-29 23:37:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
+//Set Route
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/tweets");
 
-
+//Config Dotenv
 dotenv.config();
 
-
+//Connect to MongoDB
 const uri = 'mongodb+srv://user:pass@albctwitter.mdaoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-// Prints "MongoServerError: bad auth Authentication failed."
+//Connect to Mongoose
 mongoose.connect(uri, {
   serverSelectionTimeoutMS: 5000
 }).catch(err => console.log(err.reason));
@@ -24,14 +26,18 @@ mongoose.connect(uri, {
 app.get("/",(req,res)=>{
     res.send("Welcome to ALBC Project");
 })
+
+//EHM
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+//Annotation
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
+//Listener
 app.listen(8800, () => {
   console.log("Backend server is running!");
 });
